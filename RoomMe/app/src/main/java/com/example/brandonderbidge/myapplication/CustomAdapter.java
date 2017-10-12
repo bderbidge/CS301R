85d7435849fe128e7bb52c0c6bfaa0569aba70f2
bderbidge <brander81@gmail.com>
2017-10-11 23:07:29 -0600

    quick change

--- RoomMe/app/src/main/java/com/example/brandonderbidge/myapplication/CustomAdapter.java
+++ RoomMe/app/src/main/java/com/example/brandonderbidge/myapplication/CustomAdapter.java
@@ -7,6 +7,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 /**
  * Created by brandonderbidge on 10/5/17.
  */
@@ -39,7 +41,7 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.cards_layout, parent, false);
 
-        view.setOnClickListener(MainActivity.myOnClickListener);
+        view.setOnClickListener(BuyActivity.myOnClickListener);
 
         MyViewHolder myViewHolder = new MyViewHolder(view);
         return myViewHolder;
