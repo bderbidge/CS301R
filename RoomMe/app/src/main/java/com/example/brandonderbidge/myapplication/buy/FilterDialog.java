84cb61ef57fa95b51e17e886007c76d80bb3dc30
Justin Brunner <brunner.justin.jakob@gmail.com>
2017-10-15 21:48:49 -0600

    filter dialog now correctly sets the different filter values, and marital status included

--- RoomMe/app/src/main/java/com/example/brandonderbidge/myapplication/buy/FilterDialog.java
+++ RoomMe/app/src/main/java/com/example/brandonderbidge/myapplication/buy/FilterDialog.java
@@ -24,6 +24,7 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
@@ -41,6 +42,9 @@ public class FilterDialog extends DialogFragment {
     EditText priceHigh;
     Button maleBtn;
     Button femaleBtn;
+    Button singleBtn;
+    Button marriedBtn;
+    LinearLayout selectSexContainer;
     /** The system calls this to get the DialogFragment's layout, regardless
      of whether it's being displayed as a dialog or an embedded fragment. */
     @Override
@@ -54,16 +58,36 @@ public class FilterDialog extends DialogFragment {
         priceHigh = view.findViewById(R.id.price_high);
         maleBtn = view.findViewById(R.id.male_btn);
         femaleBtn = view.findViewById(R.id.female_btn);
+        singleBtn = view.findViewById(R.id.single);
+        marriedBtn = view.findViewById(R.id.married);
+        selectSexContainer = view.findViewById(R.id.select_sex_container);
 
         filterLayout.setBackground(new ColorDrawable(Color.WHITE));
 
-        String priceLowText = "";
-        priceLowText = String.format(priceLowText, FilterModel.getInstance().getPriceLow());
+        String priceLowText = FilterModel.getInstance().getPriceLow() == null ? "" : Double.toString(FilterModel.getInstance().getPriceLow());
         priceLow.setText(priceLowText);
 
-        String priceHighText = "";
-        priceHighText = String.format(priceHighText, FilterModel.getInstance().getPriceLow());
-        priceLow.setText(priceHighText);
+        String priceHighText = FilterModel.getInstance().getPriceHigh() == null ? "" : Double.toString(FilterModel.getInstance().getPriceHigh());
+        priceHigh.setText(priceHighText);
+
+        if (FilterModel.getInstance().getSex() != null) {
+            if (FilterModel.getInstance().getSex().equalsIgnoreCase("male")) {
+                changeBtnStyle(maleBtn, R.drawable.toggle_button_left_clicked, R.color.White);
+            } else if (FilterModel.getInstance().getSex().equalsIgnoreCase("female")) {
+                changeBtnStyle(femaleBtn, R.drawable.toggle_button_left_clicked, R.color.White);
+            }
+        }
+
+        selectSexContainer.setVisibility(View.GONE);
+
+        if (FilterModel.getInstance().getMaritalStatus() != null) {
+            if (FilterModel.getInstance().getMaritalStatus().equalsIgnoreCase("single")) {
+                changeBtnStyle(singleBtn, R.drawable.toggle_button_left_clicked, R.color.White);
+                selectSexContainer.setVisibility(View.VISIBLE);
+            } else if (FilterModel.getInstance().getMaritalStatus().equalsIgnoreCase("married")) {
+                changeBtnStyle(marriedBtn, R.drawable.toggle_button_left_clicked, R.color.White);
+            }
+        }
 
         closeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -75,12 +99,15 @@ public class FilterDialog extends DialogFragment {
         maleBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FilterModel.getInstance().setSex("M");
-                maleBtn.setTextColor(ContextCompat.getColor(getContext(), R.color.White));
-                femaleBtn.setTextColor(ContextCompat.getColor(getContext(), R.color.greyedText));
 
-                maleBtn.setBackground(getResources().getDrawable(R.drawable.toggle_button_left_clicked, null));
-                femaleBtn.setBackground(getResources().getDrawable(R.drawable.toggle_button_right, null));
+                if (FilterModel.getInstance().getSex() != null && FilterModel.getInstance().getSex().equalsIgnoreCase("male")) {
+                    FilterModel.getInstance().setSex(null);
+                    changeBtnStyle(maleBtn, R.drawable.toggle_button_left, R.color.greyedText);
+                } else {
+                    FilterModel.getInstance().setSex("male");
+                    changeBtnStyle(maleBtn, R.drawable.toggle_button_left_clicked, R.color.White);
+                    changeBtnStyle(femaleBtn, R.drawable.toggle_button_right, R.color.greyedText);
+                }
 
                 ((BuyActivity) getActivity()).loadFakeData();
             }
@@ -89,12 +116,53 @@ public class FilterDialog extends DialogFragment {
         femaleBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FilterModel.getInstance().setSex("F");
-                femaleBtn.setTextColor(ContextCompat.getColor(getContext(), R.color.White));
-                maleBtn.setTextColor(ContextCompat.getColor(getContext(), R.color.greyedText));
+                if (FilterModel.getInstance().getSex() != null && FilterModel.getInstance().getSex().equalsIgnoreCase("female")) {
+                    FilterModel.getInstance().setSex(null);
+                    changeBtnStyle(femaleBtn, R.drawable.toggle_button_right, R.color.greyedText);
+                } else {
+                    FilterModel.getInstance().setSex("female");
+                    changeBtnStyle(femaleBtn, R.drawable.toggle_button_right_clicked, R.color.White);
+                    changeBtnStyle(maleBtn, R.drawable.toggle_button_left, R.color.greyedText);
+                }
 
-                femaleBtn.setBackground(getResources().getDrawable(R.drawable.toggle_button_right_clicked, null));
-                maleBtn.setBackground(getResources().getDrawable(R.drawable.toggle_button_left, null));
+                ((BuyActivity) getActivity()).loadFakeData();
+            }
+        });
+
+        singleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (FilterModel.getInstance().getMaritalStatus() != null
+                        && FilterModel.getInstance().getMaritalStatus().equalsIgnoreCase("single")) {
+                    FilterModel.getInstance().setMaritalStatus(null);
+                    changeBtnStyle(singleBtn, R.drawable.toggle_button_left, R.color.greyedText);
+                    selectSexContainer.setVisibility(View.GONE);
+                } else {
+                    FilterModel.getInstance().setMaritalStatus("single");
+                    changeBtnStyle(singleBtn, R.drawable.toggle_button_left_clicked, R.color.White);
+                    changeBtnStyle(marriedBtn, R.drawable.toggle_button_right, R.color.greyedText);
+                    selectSexContainer.setVisibility(View.VISIBLE);
+                }
+
+                ((BuyActivity) getActivity()).loadFakeData();
+            }
+        });
+
+        marriedBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (FilterModel.getInstance().getMaritalStatus() != null
+                        && FilterModel.getInstance().getMaritalStatus().equalsIgnoreCase("married")) {
+                    FilterModel.getInstance().setMaritalStatus(null);
+                    changeBtnStyle(marriedBtn, R.drawable.toggle_button_right, R.color.greyedText);
+                    selectSexContainer.setVisibility(View.VISIBLE);
+                } else {
+                    FilterModel.getInstance().setMaritalStatus("married");
+                    changeBtnStyle(marriedBtn, R.drawable.toggle_button_right_clicked, R.color.White);
+                    changeBtnStyle(singleBtn, R.drawable.toggle_button_left, R.color.greyedText);
+                    selectSexContainer.setVisibility(View.GONE);
+
+                }
 
                 ((BuyActivity) getActivity()).loadFakeData();
             }
@@ -152,16 +220,21 @@ public class FilterDialog extends DialogFragment {
     }
 
     private void updatePriceLow() {
-        String priceLowText = (priceLow.getText().toString().equals("")) ? "0" : priceLow.getText().toString();
-        FilterModel.getInstance().setPriceHigh(Double.parseDouble(priceLowText));
+        Double priceLowText = (priceLow.getText().toString().equals("")) ? null : Double.parseDouble(priceLow.getText().toString());
+        FilterModel.getInstance().setPriceLow(priceLowText);
 
         ((BuyActivity) getActivity()).loadFakeData();
     }
 
     private void updatePriceHigh() {
-        String priceHighText = (priceHigh.getText().toString().equals("")) ? "100000" : priceHigh.getText().toString();
-        FilterModel.getInstance().setPriceHigh(Double.parseDouble(priceHighText));
+        Double priceHighText = (priceHigh.getText().toString().equals("")) ? null : Double.parseDouble(priceHigh.getText().toString());
+        FilterModel.getInstance().setPriceHigh(priceHighText);
 
         ((BuyActivity) getActivity()).loadFakeData();
+    }
+
+    private void changeBtnStyle(Button btn, int drawable, int  textColor) {
+        btn.setTextColor(ContextCompat.getColor(getContext(), textColor));
+        btn.setBackground(getResources().getDrawable(drawable, null));
     }
 }
