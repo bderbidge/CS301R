1a4d3132fb9af21b14658186855d4992bcb889a8
Justin Brunner <brunner.justin.jakob@gmail.com>
2017-10-18 23:38:43 -0600

    stuff for new contract

---
+++ RoomMe/app/src/main/java/com/example/brandonderbidge/myapplication/sell/DatePickerDialog.java
@@ -0,0 +1,100 @@
+package com.example.brandonderbidge.myapplication.sell;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.example.brandonderbidge.myapplication.R;
+import com.example.brandonderbidge.myapplication.main.MainController;
+
+/**
+ * Created by justinbrunner on 10/12/17.
+ */
+
+public class DatePickerDialog extends DialogFragment {
+    private String TAG = "DatePickerDialog";
+    private DatePicker datePicker;
+    private MainController mainController;
+    private Button cancelBtn;
+    private Button okBtn;
+
+    public void setMainController(MainController mainController) {
+        this.mainController = mainController;
+    }
+
+    /** The system calls this to get the DialogFragment's layout, regardless
+     of whether it's being displayed as a dialog or an embedded fragment. */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialog_datepicker, container, false);
+
+        view.setBackgroundColor(getResources().getColor(R.color.White));
+
+        datePicker = view.findViewById(R.id.date_picker);
+        okBtn = view.findViewById(R.id.ok_datepicker_btn);
+        cancelBtn = view.findViewById(R.id.cancel_datepicker_btn);
+
+        okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String date = datePicker.getMonth() + "/" + datePicker.getDayOfMonth() + "/" + datePicker.getYear();
+                ((NewContractFragment) getActivity().getSupportFragmentManager().findFragmentByTag(getString(R.string.TAG_new_contract)))
+                        .setDate(date);
+                dismiss();
+            }
+        });
+
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
+
+
+        return view;
+    }
+
+    /** The system calls this only when creating the layout in a dialog. */
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        return dialog;
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        super.onDismiss(dialog);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.close_btn) {
+            dismiss();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void changeBtnStyle(Button btn, int drawable, int  textColor) {
+        btn.setTextColor(ContextCompat.getColor(getContext(), textColor));
+        btn.setBackground(getResources().getDrawable(drawable, null));
+    }
+}
--- RoomMe/app/src/main/java/com/example/brandonderbidge/myapplication/sell/NewContractFragment.java
+++ RoomMe/app/src/main/java/com/example/brandonderbidge/myapplication/sell/NewContractFragment.java
@@ -1,22 +1,34 @@
 package com.example.brandonderbidge.myapplication.sell;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.brandonderbidge.myapplication.R;
 import com.example.brandonderbidge.myapplication.main.MainController;
 import com.example.brandonderbidge.myapplication.model.Contract;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 /**
  * Created by justinbrunner on 10/16/17.
@@ -25,6 +37,14 @@ import java.util.ArrayList;
 public class NewContractFragment extends Fragment {
     private String TAG = "NewContract";
     private MainController mainController;
+    Spinner maritalStatusSpinner;
+    Spinner sexSpinner;
+    Button addImage;
+    EditText sellBy;
+    EditText dateAvailable;
+    boolean isSellByDateFrag;
+
+    private static int RESULT_LOAD_IMAGE = 1;
 
     public void setMainController(MainController mainController) {
         this.mainController = mainController;
@@ -40,6 +60,73 @@ public class NewContractFragment extends Fragment {
         if (ab != null) {
             ab.setDisplayHomeAsUpEnabled(true);
         }
+
+        view.setBackgroundColor(Color.WHITE);
+
+        maritalStatusSpinner = view.findViewById(R.id.marital_status_spinner);
+        sexSpinner = view.findViewById(R.id.sex_spinner);
+        addImage = view.findViewById(R.id.add_image);
+        sellBy = view.findViewById(R.id.sell_by);
+        dateAvailable = view.findViewById(R.id.date_available);
+
+        ArrayAdapter<CharSequence> maritalAdapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.marital_status_list, R.layout.spinner_item);
+        maritalAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        maritalStatusSpinner.setAdapter(maritalAdapter);
+
+        ArrayAdapter<CharSequence> sexAdapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.sex_list, R.layout.spinner_item);
+        sexAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sexSpinner.setAdapter(sexAdapter);
+
+        maritalStatusSpinner.setSelection(1);
+
+        sellBy.setShowSoftInputOnFocus(false);
+        dateAvailable.setShowSoftInputOnFocus(false);
+
+        sellBy.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                closeKeyboard();
+                isSellByDateFrag = true;
+                showDateDialog();
+                return false;
+            }
+        });
+
+        dateAvailable.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                closeKeyboard();
+                isSellByDateFrag = false;
+                showDateDialog();
+                return false;
+            }
+        });
+
+        maritalStatusSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (maritalStatusSpinner.getSelectedItem().toString().equals("Single")) {
+                    sexSpinner.setEnabled(true);
+                } else {
+                    sexSpinner.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {}
+        });
+
+        addImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(
+                        Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
 
         getActivity().setTitle(R.string.new_contract);
 
@@ -66,5 +153,37 @@ public class NewContractFragment extends Fragment {
         }
 
         getFragmentManager().popBackStack();
+    }
+
+    public void showDateDialog() {
+
+        if (getActivity().findViewById(R.id.datepicker_dialog_container) == null) {
+            FragmentManager fragmentManager = getFragmentManager();
+            DatePickerDialog datePickerDialog = new DatePickerDialog();
+
+            datePickerDialog.setMainController(mainController);
+
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+            transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+            transaction.add(android.R.id.content, datePickerDialog)
+                    .commit();
+        }
+    }
+
+    public void setDate(String date) {
+        if (isSellByDateFrag) {
+            sellBy.setText(date);
+        } else {
+            dateAvailable.setText(date);
+        }
+    }
+
+    private void closeKeyboard() {
+        InputMethodManager inputManager = (InputMethodManager)
+                getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        inputManager.hideSoftInputFromWindow(getActivity().getCurrentFocus() == null ? null : getActivity().getCurrentFocus().getWindowToken(),
+                InputMethodManager.HIDE_NOT_ALWAYS);
     }
 }
